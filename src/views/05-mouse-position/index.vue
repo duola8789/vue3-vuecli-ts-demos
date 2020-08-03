1af47c413ff6733c25e1e5e71424f7f3d42f049f
zhouhao15 <zhouhao15@baidu.com>
2020-08-03 10:36:23 +0800

    feat: demo5

---
+++ src/views/05-mouse-position/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mouse-position-container">
+    <h1 class="page-title">追踪鼠标位置</h1>
+    <div class="page-content">
+      <div class="page-content-item">
+        <p>Now the mouse is at: {{ pos }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref, onMounted, onUnmounted, computed} from 'vue';
+
+function useMousePosition() {
+  const x = ref(0);
+  const y = ref(0);
+
+  function update(e) {
+    x.value = e.pageX;
+    y.value = e.pageY;
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', update);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', update);
+  });
+
+  return {x, y};
+}
+
+export default {
+  name: 'MousePosition',
+  setup() {
+    const {x, y} = useMousePosition();
+
+    const pos = computed(() => `(${x.value}, ${y.value})`);
+
+    return {pos};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mouse-position-container {
+}
+</style>
