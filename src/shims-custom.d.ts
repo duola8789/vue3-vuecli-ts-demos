0fe60ece14ca7c89d6d8be1c3cd53f05dce4b2d8
zhouhao15 <zhouhao15@baidu.com>
2021-05-12 18:40:06 +0800

    feat: 添加vue-router的meta类型规定

--- src/shims-custom.d.ts
+++ src/shims-custom.d.ts
@@ -1,9 +1,19 @@
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 import Vue from 'vue';
+import 'vue-router';
 
 // Vue 原型上添加的东西，需要在此定义
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $sayHello: () => void;
+  }
+}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    // 是可选的
+    isAdmin?: boolean;
+    // 每个路由都必须声明
+    label: string;
   }
 }
