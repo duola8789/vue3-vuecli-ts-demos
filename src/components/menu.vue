e649533cfde1be821eabaed6c85afe7428b0ef59
zhouhao15 <zhouhao15@baidu.com>
2021-05-17 16:43:12 +0800

    feat: 优化菜单

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,22 +1,34 @@
 <template>
   <ul class="menu-container">
-    <li v-for="(menu, index) in menus" :key="menu.path + index" class="menu-item">
-      <router-link :to="menu.path">{{ menu.meta.label }}</router-link>
+    <li v-for="(menu, index) in state.menus" :key="menu.path + index" class="menu-item">
+      <p class="menu-item-link" :class="isActive(menu) ? 'router-link-active' : ''" @click="onClick(menu)">
+        {{ menu.meta.label }}
+      </p>
     </li>
   </ul>
 </template>
 
-<script>
+<script lang="ts">
 import menus from '@/router/config';
+import {defineComponent, reactive} from 'vue';
+import {RouteRecordRaw, useRoute, useRouter} from 'vue-router';
 
-export default {
+export default defineComponent({
   name: 'Menu',
-  data() {
-    return {
-      menus: menus
+  setup() {
+    const state = reactive<{menus: RouteRecordRaw[]}>({menus: menus.filter((v) => !!v.meta)});
+    const router = useRouter(),
+      route = useRoute();
+
+    const onClick = (menu: RouteRecordRaw) => {
+      router.push(menu.path);
     };
+
+    const isActive = (menu: RouteRecordRaw) => route.path === menu.path;
+
+    return {state, onClick, isActive};
   }
-};
+});
 </script>
 
 <style scoped lang="scss">
@@ -35,6 +47,15 @@ export default {
 
     .router-link-active {
       color: #42b983;
+    }
+
+    .menu-item-link {
+      cursor: pointer;
+      transition: all 0.1s linear;
+
+      &:not(.router-link-active):hover {
+        color: #409eff;
+      }
     }
   }
 }
